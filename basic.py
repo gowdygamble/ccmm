f4ea2dd8120f64ee5cee2f8ce625e5b5fa8dbb25
Paul <paulgamble49@gmail.com>
2023-02-03 09:52:29 -0800

    first attempt at video

--- basic.py
+++ basic.py
@@ -7,12 +7,12 @@ def create_blank_frame(size=32):
     return blank_frame
 
 def draw_circle(img):
-    cv2.circle(img, (16,16), 12, (255,0,0), lineType=8, shift=0, thickness=-1) #thickness=1, 
+    cv2.circle(img, (16,16), 12, (255,0,0), lineType=8, thickness=-1) #thickness=1, 
     return img
 
-z = create_blank_frame()
-z = draw_circle(z)
+def create_frame_stack(nb_frames, size=32):
+    frame_stack = [create_blank_frame(size) for _ in range(nb_frames)]
+    return frame_stack
 
-plt.figure()
-plt.imshow(z)
-plt.show()
+frames = create_frame_stack(10)
+print(len(frames))
