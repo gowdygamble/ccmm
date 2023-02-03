119885b64bf0df59bd1c07c0aaeceed2743d14a9
Paul <paulgamble49@gmail.com>
2023-02-03 10:02:54 -0800

    display frames

--- basic.py
+++ basic.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import math
 
 def create_blank_frame(size=32):
     blank_frame=np.zeros((size,size,3))
@@ -14,5 +15,18 @@ def create_frame_stack(nb_frames, size=32):
     frame_stack = [create_blank_frame(size) for _ in range(nb_frames)]
     return frame_stack
 
+def display_frame_stack(frames, cols=5):
+
+    fig, ax = plt.subplots(2, cols)
+
+    for i in range(len(frames)):
+        frame = frames[i]
+        r = math.floor(i/cols)
+        c = i%cols
+        ax[r,c].imshow(frame)
+
+    plt.show()
+
 frames = create_frame_stack(10)
 print(len(frames))
+display_frame_stack(frames)
