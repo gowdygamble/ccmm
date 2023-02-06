5ad00a19e31bd3ef6cfa30db1482b18838aceea7
Paul <paulgamble49@gmail.com>
2023-02-06 13:40:12 -0800

    add square function

--- basic.py
+++ basic.py
@@ -11,6 +11,10 @@ def draw_circle(img):
     cv2.circle(img, (16,16), 12, (255,0,0), lineType=8, thickness=-1) #thickness=1, 
     return img
 
+def draw_square(img):
+    cv2.rectangle(img, (8,8), (20,2), (0,0,255), thickness=-1)
+    return img
+
 def create_frame_stack(nb_frames, size=32):
     frame_stack = [create_blank_frame(size) for _ in range(nb_frames)]
     return frame_stack
@@ -38,7 +42,24 @@ def move_circle_up(frames):
 
     return frames
 
+def draw_shape_from_function(frame, shape_func):
+    return shape_func(frame)
+
+def alternate_circles_squares(frames):
+    for i in range(len(frames)):
+        if i%2==0:
+            f = draw_circle
+        else:
+            f = draw_square
+        frames[i] = draw_shape_from_function(frames[i], f)
+    return frames
+
+
 frames = create_frame_stack(10)
 frames = move_circle_up(frames)
 print(len(frames))
+display_frame_stack(frames)
+
+frames = create_frame_stack(10)
+frames = alternate_circles_squares(frames)
 display_frame_stack(frames)
