64ed191428ae3cb5a93076ad09dadf04deb5a86a
Paul <paulgamble49@gmail.com>
2023-02-07 20:54:18 -0800

    write plan for dataset gen

--- generate_dataset.py
+++ generate_dataset.py
@@ -3,3 +3,12 @@ import captions
 z = len(captions.shapes) * len(captions.directions) * len(captions.colors)
 
 print("Total combinations:", z)
+
+# plan:
+# iterate over all combinations
+# up to some maximum in case it gets crazy
+# create the caption
+# create the video
+# save both with some id
+# write id + combo to a csv
+# can just write the caption to the csv...
