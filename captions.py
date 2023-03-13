db84b1cda2b5ec3d18882063fe8f096766949562
Paul <paulgamble49@gmail.com>
2023-03-13 10:08:53 -0700

    refactor constant values

--- captions.py
+++ captions.py
@@ -1,28 +1,7 @@
 import random
 
+from constants import *
 
-color_rgb_values = {
-    "red": (255,0,0),
-    "blue": (0,0,255),
-    "green": (0,255,0),
-    "yellow": (255,0,0),
-    "orange": (255,0,0),
-    "purple": (255,0,0)
-}
-
-shapes = [
-    "circle",
-    "square",
-    "triangle"
-]
-
-
-direction_vectors = {
-    "up": (0,-1),
-    "down": (0,1),
-    "left": (-1,0),
-    "right": (1,0)
-}
 
 class background:
     def __init__(self, color):
---
+++ constants.py
@@ -0,0 +1,24 @@
+
+
+color_rgb_values = {
+    "red": (255,0,0),
+    "blue": (0,0,255),
+    "green": (0,255,0),
+    "yellow": (255,0,0),
+    "orange": (255,0,0),
+    "purple": (255,0,0)
+}
+
+shapes = [
+    "circle",
+    "square",
+    "triangle"
+]
+
+
+direction_vectors = {
+    "up": (0,-1),
+    "down": (0,1),
+    "left": (-1,0),
+    "right": (1,0)
+}
